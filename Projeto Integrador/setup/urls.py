84a7c83dcf750220c65e4cdac216648219935541
Vitória Maria Bezerra Pinheiro <vitoriamariabezerra72@gmail.com>
2026-05-31 17:07:17 -0300

    Delete Projeto Integrador directory

--- Projeto Integrador/setup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Redireciona tudo para as rotas do freelacademy
-    path('', include('freelacademy.urls')), 
-]
